935e9fc297b104cc7eb0e7f813ad2894d166bd68
Rakshith J <rakshithj07@gmail.com>
2022-04-11 23:00:03 +0530

    designs added

--- DesktopApp/Objective5.py
+++ DesktopApp/Objective5.py
@@ -19,16 +19,8 @@ class App:
         GLabel_189["font"] = ft
         GLabel_189["fg"] = "#333333"
         GLabel_189["justify"] = "center"
-        GLabel_189["text"] = "Student Performance in "
-        GLabel_189.place(x=0,y=0,width=589,height=87)
-
-        GLabel_635=tk.Label(root)
-        ft = tkFont.Font(family='Times',size=48)
-        GLabel_635["font"] = ft
-        GLabel_635["fg"] = "#333333"
-        GLabel_635["justify"] = "center"
-        GLabel_635["text"] = "a Subject"
-        GLabel_635.place(x=150,y=80,width=274,height=59)
+        GLabel_189["text"] = "Semester-Wise"
+        GLabel_189.place(x=0,y=10,width=590,height=59)
 
         GLabel_312=tk.Label(root)
         ft = tkFont.Font(family='Times',size=33)
@@ -36,7 +28,7 @@ class App:
         GLabel_312["fg"] = "#333333"
         GLabel_312["justify"] = "center"
         GLabel_312["text"] = "Subject code :-"
-        GLabel_312.place(x=20,y=150,width=286,height=117)
+        GLabel_312.place(x=30,y=130,width=286,height=117)
 
         GLineEdit_856=tk.Entry(root)
         GLineEdit_856["borderwidth"] = "1px"
@@ -45,7 +37,7 @@ class App:
         GLineEdit_856["fg"] = "#333333"
         GLineEdit_856["justify"] = "center"
         GLineEdit_856["text"] = "Enter code"
-        GLineEdit_856.place(x=310,y=190,width=197,height=41)
+        GLineEdit_856.place(x=320,y=170,width=197,height=41)
 
         GButton_229=tk.Button(root)
         GButton_229["bg"] = "#efefef"
@@ -54,8 +46,16 @@ class App:
         GButton_229["fg"] = "#000000"
         GButton_229["justify"] = "center"
         GButton_229["text"] = "Visualize"
-        GButton_229.place(x=200,y=270,width=173,height=45)
+        GButton_229.place(x=200,y=260,width=173,height=45)
         GButton_229["command"] = self.GButton_229_command
+
+        GLabel_637=tk.Label(root)
+        ft = tkFont.Font(family='Times',size=48)
+        GLabel_637["font"] = ft
+        GLabel_637["fg"] = "#333333"
+        GLabel_637["justify"] = "center"
+        GLabel_637["text"] = "Performance Analysis"
+        GLabel_637.place(x=0,y=30,width=594,height=129)
 
     def GButton_229_command(self):
         print("command")
